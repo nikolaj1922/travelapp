79276ee6bcb07b2a8c69018b2415230b45a6ea40
Nik Bond <bna.career@mail.ru>
2023-03-30 22:04:14 +0600

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,51 +2,16 @@ import { FC } from "react";
 
 const Footer: FC = () => {
   return (
-    <div className="fixed bottom-0 w-full border border-gray-300 px-32 py-4 bg-gray-100 text-gray-600 flex justify-between">
-      {/* <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">LOREM</h5>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">LOREM</h5>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">LOREM</h5>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">LOREM</h5>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-        <p>Lorem</p>
-      </div> */}
-      <div className="w-2/3 flex justify-between">
-        <span className="inline hover:underline cursor-pointer">
-          © 2023 Airbnb, Inc.
-        </span>
-        <span className="hover:underline cursor-pointer">Confidentiality</span>
-        <span className="hover:underline cursor-pointer">Conditions</span>
-        <span className="hover:underline cursor-pointer">Site Map</span>
-        <span className="hover:underline cursor-pointer">Company details</span>
-      </div>
-      <div>
-        <span className="font-bold">Created By Nikolay Bondarenko</span>
-      </div>
+    <div className=" w-full border border-gray-300 py-4 bg-gray-100 text-gray-600 flex flex-col items-start sm:flex-row sm:items-center space-y-3 px-8 md:px-12 lg:px-24 xl:px-32  flex-wrap md:fixed md:bottom-0 sm:justify-between">
+      <span className="hover:underline cursor-pointer">2023 Airbnb, Inc.</span>
+      <span className="hover:underline cursor-pointer !m-0">
+        Confidentiality
+      </span>
+      <span className="hover:underline cursor-pointer !m-0">Conditions</span>
+      <span className="hover:underline cursor-pointer !m-0">Site Map</span>
+      <span className="hover:underline cursor-pointer !m-0">
+        Company details
+      </span>
     </div>
   );
 };
